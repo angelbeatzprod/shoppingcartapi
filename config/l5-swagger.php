a4d9783b2d3138e31a14c463215eb7d4758da112
angelbeatzprod <angelbeatzprod@gmail.com>
2021-05-04 01:12:07 +0300

    c-5: Commented out auth for docs

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -57,19 +57,19 @@ return [
             */
             'middleware' => [
                 'api' => [
-                    // /*
-                    // |--------------------------------------------------------------------------
-                    // | Edit to set the api's title
-                    // |--------------------------------------------------------------------------
-                    // */
-                    \App\Http\Middleware\EncryptCookies::class,
-                    \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-                    \Illuminate\Session\Middleware\StartSession::class,
-                    \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-                    \App\Http\Middleware\VerifyCsrfToken::class,
-                    \Illuminate\Routing\Middleware\SubstituteBindings::class,
-                    \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
-                    'auth',
+                    // // /*
+                    // // |--------------------------------------------------------------------------
+                    // // | Edit to set the api's title
+                    // // |--------------------------------------------------------------------------
+                    // // */
+                    // \App\Http\Middleware\EncryptCookies::class,
+                    // \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+                    // \Illuminate\Session\Middleware\StartSession::class,
+                    // \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+                    // \App\Http\Middleware\VerifyCsrfToken::class,
+                    // \Illuminate\Routing\Middleware\SubstituteBindings::class,
+                    // \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
+                    // 'auth',
                 ],
                 'asset' => [],
                 'docs' => [],
